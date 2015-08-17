9ec002115825d77587ddfce29dbc2bdfa66c3a8f
root <root@localhost.localdomain>
2015-08-17 17:47:39 +0200

    botnet temp added

--- botnetsearch.py
+++ botnetsearch.py
@@ -1,7 +1,9 @@
+
 import pycurl
 import os
 import commands
-import libcurl
+#import libcurl
+import sys
 
 '''
 Nohidy is a free software you could redistribute it and/or modify it under the terms of the 
@@ -19,12 +21,32 @@ __maintainer__ = "Filip kalebo"
 __email__ = "flipchan@riseup.net"
 __status__ = "still in developing"
 '''
+                                                        
+#beforescan =raw_input("master do you want me to run an admin finder programm to help find the admin panel of botnet first?Yes/no: ")
+#      if beforescan=="Yes":
+#            try: 
+#                  os.system("python adminfind.py")
+                  
 
 print""
 print"Botnet search tool / Botnet type finder by flipchan"
 
-beforescan =raw_input("master do you want me to run an admin finder programm to help find the admin panel of botnet first?Yes/no: ")
-      if beforescan=="Yes":
-            try: 
-                  os.system("python adminfind.py")
-                  
+#temp
+print"i am on a pentest and have set up a temporary selution for you "
+print"try out these open source botnet tools until Nohidy gots its mojo uploaded n patched"
+print" BAMF Botnet Analysis Modular Framework and buttinsky an open source framework for automated botnet monitoring" 
+print"enter BAMF and buttinsky"
+def main():
+                                                                           tempbotnet =raw_input("Do you want me to install BAMF and buttinsky: ")
+                                                                           print"Downloading " + tempbotnet                                                                           
+                                                                           if tempbotnet=="BAMF":
+                                                                           
+                                                                                                                                                      os.system("git clone https://github.com/bwall/BAMF.git &&")  
+                                                                          
+                                                                           if tempbotnet=="buttinsky":
+                                                                                                                                                      os.system(" git clone https://github.com/buttinsky/buttinsky.git")
+
+
+
+
+main()

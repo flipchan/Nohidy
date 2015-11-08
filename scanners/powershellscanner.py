e5b469a12f988ba50d932e855022cb21b3782ac0
root <root@localhost.localdomain>
2015-11-08 17:28:35 +0100

    added powershell

---
+++ scanners/powershellscanner.py
@@ -0,0 +1,10 @@
+import sys, os
+#Part of Nohidy
+#written by Flipchan
+print"Part of Nohidy framework/toolkit"
+print"printing all Processes running locking"
+print""
+os.system("powershell Get-Process")
+print""
+print"look for weird stuff = virus "
+exte =raw_input(" press enter to exit ")

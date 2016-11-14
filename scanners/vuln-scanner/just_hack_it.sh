c31117b8b1bc73d5f0d166bf9cb239b4537e1466
flipchan <flipchan@users.noreply.github.com>
2016-11-14 11:22:58 +0100

    Update just_hack_it.sh

--- scanners/vuln-scanner/just_hack_it.sh
+++ scanners/vuln-scanner/just_hack_it.sh
@@ -4,11 +4,15 @@ echo  'auto-scripting file that runs all kinds of scanner against target'
 echo -n "Enter target ip or domain? "
 read answer
 $target = $answer
+echo -n 'Enter wordlist for url bruteforce'
+read answer
+$wordlist = $answer
 
 echo 'starting enumeration'
-whois $target > whois.log &
-fierce -dns $target > fierce.log &
+whois $target > whois.$target.log &
+fierce -dns $target > fierce.$target.log &
+dirb $target $wordlist > dirb.$target.log &
 
 echo 'starting vuln scanners'
-golismero $target > golismero.log &
-nikto -h $target -C all > nikto.log &
+golismero $target > golismero.$target.log &
+nikto -h $target -C all > nikto.$target.log &

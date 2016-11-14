160d60f67935e783f5feaceffcf5dc8cb6effa69
flipchan <flipchan@users.noreply.github.com>
2016-11-14 11:01:09 +0100

    Update just_hack_it.sh

--- scanners/vuln-scanner/just_hack_it.sh
+++ scanners/vuln-scanner/just_hack_it.sh
@@ -5,6 +5,10 @@ echo -n "Enter target ip or domain? "
 read answer
 $target = $answer
 
-echo 'starting'
+echo 'starting enumeration'
+whois $target
+fierce -dns $target
+
+echo 'starting vuln scanners'
 golismero $target > golismero.log &
 nikto -h $target -C all > nikto.log &

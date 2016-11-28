d622d2a638fd00a289c07a853abd27e8e231d1ef
flipchan <flipchan@users.noreply.github.com>
2016-11-28 12:44:02 +0100

    Create mac_defender.sh

---
+++ scanners/mac_defender.sh
@@ -0,0 +1,7 @@
+echo ’lets go A-wall’
+echo ' ' > /Users/uruser/.ssh/known_hosts
+echo 'cleaned ssh'
+echo ' ' > /Users/uruser/.bash_history 
+echo 'bash cleaned'
+echo ' ' > /.Spotlight-V100
+echo ’meta clean’

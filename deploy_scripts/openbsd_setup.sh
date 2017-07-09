bbbe0538eae421c342ef1a17b4df3dc4e7f01b49
flipchan <flipchan@users.noreply.github.com>
2017-07-09 12:52:11 +0100

    Update openbsd_setup.sh

--- deploy_scripts/openbsd_setup.sh
+++ deploy_scripts/openbsd_setup.sh
@@ -1,18 +1,21 @@
 echo 'setting up openbsd'
 
-pkg_add -v gnupg-2.1.15p2 gmake privoxy dante wget libevent
+pkg_add -v gnupg-2.1.15p2 gmake privoxy dante wget libevent git
 ln -sf /usr/local/bin/gpg2 /usr/local/bin/gpg
 echo 'installing ToR'
 mkdir tor 
 cd tor 
 echo 'make sure u update to the latest tor installation check out the latest one from https://dist.torproject.org/'
-wget https://dist.torproject.org/tor-0.3.0.8.tar.gz.asc
-gpg --verify tor-0.3.0.8.tar.gz.asc
-wget https://dist.torproject.org/tor-0.3.0.8.tar.gz
-tar -zxvf tor-0.3.0.8.tar.gz
-cd tor-0.3.0.8/
+wget https://dist.torproject.org/tor-0.3.0.9.tar.gz.asc
+gpg --verify tor-0.3.0.9.tar.gz.asc
+wget https://dist.torproject.org/tor-0.3.0.9.tar.gz
+tar -zxvf tor-0.3.0.9.tar.gz
+cd tor-0.3.0.9/
 ./configure 
-gmake
-
+make
+make install
+cd ../
+echo 'Tor is installed'
+echo 'check out https://www.torproject.org/download/download-unix.html.en'
 
 echo 'tor is installed here is another cool thing https://trac.torproject.org/projects/tor/wiki/doc/OpenbsdChrootedTor'
